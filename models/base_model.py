31958130d3dfc11895c549eb2a27e7ffdbc95da9
VeritasYin <veritaspotter@gmail.com>
2020-12-10 20:21:54 -0500

    add node comparision

--- models/base_model.py
+++ models/base_model.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn as nn
 import layers.layers as layers
 import layers.modules as modules
+import pdb
 
 
 class BaseModel(nn.Module):
@@ -10,9 +11,11 @@ class BaseModel(nn.Module):
         Build the model computation graph, until scores/values are returned at the end
         """
         super().__init__()
-
+        self.bins  = 8
         self.config = config
+        self.device = torch.device(config.device if torch.cuda.is_available() else 'cpu')
         self.is_SimGNN = config.dataset_name in ['LINUX', 'AIDS700nef']
+        self.use_NTN = False
         use_new_suffix = config.architecture.new_suffix  # True or False
         block_features = config.architecture.block_features  # List of number of features in each regular block
         original_features_num = config.node_labels + 1  # Number of features of the input
@@ -44,11 +47,33 @@ class BaseModel(nn.Module):
             
         # SimGNN layers (graph rep -> ged)
         if self.is_SimGNN:
-            self.SimGNN_NTN = modules.NTN(self.config.num_classes, 256)
+#             self.SimGNN_NTN = modules.NTN(self.config.num_classes, 256)
+#             self.SimGNN_layers = nn.ModuleList()
+#             self.SimGNN_layers.append(modules.FullyConnected(256, 128)) 
+#             self.SimGNN_layers.append(modules.FullyConnected(128, 64)) 
+#             self.SimGNN_layers.append(modules.FullyConnected(64, 1, activation_fn=None)) 
             self.SimGNN_layers = nn.ModuleList()
+            self.SimGNN_layers.append(modules.FullyConnected((self.config.num_classes+self.bins)*2, 256)) 
             self.SimGNN_layers.append(modules.FullyConnected(256, 128)) 
-            self.SimGNN_layers.append(modules.FullyConnected(128, 64)) 
-            self.SimGNN_layers.append(modules.FullyConnected(64, 1, activation_fn=None)) 
+            self.SimGNN_layers.append(modules.FullyConnected(128, 1, activation_fn=None))
+            
+    def calculate_histogram(self, matf1, matf2):
+        """
+        Calculate histogram from similarity matrix.
+        :param matf1: Feature matrix for type 1.
+        :param matf2: Feature matrix for type 2.
+        :return hist: Histsogram of similarity scores.
+        """
+        bs, F, M = matf1.shape
+        nbs = bs**2
+        hist = torch.empty(nbs, self.bins*2, device=self.device)
+        input1 = matf1.expand(bs,bs,F,M)
+        input2 = matf2.expand(bs,bs,F,M)
+        scores1 = torch.matmul(torch.transpose(torch.transpose(input1, 0, 1), -2, -1), input1).view(nbs, -1).detach()
+        scores2 = torch.matmul(torch.transpose(torch.transpose(input2, 0, 1), -2, -1), input2).view(nbs, -1).detach()
+        for i in range(nbs):
+            hist[i] = torch.cat((nn.functional.normalize(torch.histc(scores1[i], bins=self.bins), p=2, dim=0),nn.functional.normalize(torch.histc(scores2[i], bins=self.bins), p=2, dim=0)), dim = -1)
+        return hist
 
     def forward(self, input):
         x = input
@@ -64,6 +89,10 @@ class BaseModel(nn.Module):
 
         if not self.config.architecture.new_suffix:
             # old suffix
+            bs = x.shape[0]
+            x_max = torch.max(x, dim=2)[0]
+            x_sum = torch.sum(x, dim=2)
+            hist = self.calculate_histogram(x_max, x_sum)
             x = layers.diag_offdiag_maxpool(x)  # NxFxMxM -> Nx2F
             for fc in self.fc_layers:
                 x = fc(x)
@@ -72,10 +101,18 @@ class BaseModel(nn.Module):
         # If SimGNN, pair up and apply anothe 3-layer MLP
         # Afterwards scores has shape (n,n)
         if self.is_SimGNN:
-            n = scores.shape[0]
-            x = self.SimGNN_NTN(scores)
+            if self.use_NTN:
+                x = self.SimGNN_NTN(scores)
+                x = torch.cat((x, hist), -1)
+            else:
+                n = scores.shape[0]
+                d = scores.shape[1]
+                input1 = scores.expand(n,n,d)
+                input2 = torch.transpose(input1,0,1)
+                x = torch.reshape(torch.cat((input1,input2), 2) , (n*n,2*d))
+                x = torch.cat((x, hist), -1)
             for fc in self.SimGNN_layers:
                 x = fc(x)
-            scores = torch.reshape(x, (n,n))
+            geds = torch.reshape(x, (n,n))
 
-        return scores
+        return geds
